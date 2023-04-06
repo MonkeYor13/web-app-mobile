451dcd1860cab828cdfac88dc1ea2a5cddb55cc5
“MonkeYor13” <monkeyor13@gmail.com>
2023-04-06 17:51:48 -0300

    configuracion de fuentes y aplicando tailwind

---
+++ app.js
@@ -0,0 +1,13 @@
+
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        alkatra: ["Alkatra", "cursive"],
+      },
+      colors: {
+        clifford: "#da373d",
+      },
+    },
+  },
+};
